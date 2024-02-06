8525d48364041aaaf8c378f92302bb93ff680986
양희준 <137030946+heejun8250@users.noreply.github.com>
2024-02-06 19:18:51 +0900

    [level 0] Title: 특별한 이차원 배열 1, Time: 0.18 ms, Memory: 37.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181833. 특별한 이차원 배열 1/특별한 이차원 배열 1.js
@@ -0,0 +1,11 @@
+function solution(n) {
+    var answer = []
+    for (let i = 0; i < n; i++) {
+        answer.push(Array(n).fill(0))
+    }
+
+    for (let j = 0; j < n; j++) {
+        answer[j][j] = 1;
+    }
+    return answer;
+}
